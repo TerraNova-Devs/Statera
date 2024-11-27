d0d5f45e104a699793c7f97c1d2a79a01711d5b1
derbxsti <contact@bastitime.dev>
2024-11-27 22:10:35 +0100

    Fixed Anvil bugs and moved to new AnvilView

--- src/main/java/org/statera/listener/PrepareAnvilListener.java
+++ src/main/java/org/statera/listener/PrepareAnvilListener.java
@@ -2,7 +2,6 @@ package org.statera.listener;
 
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -10,30 +9,26 @@ import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
 import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.inventory.meta.ItemMeta;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.bukkit.Bukkit.getServer;
+import org.bukkit.inventory.meta.Repairable;
+import org.bukkit.inventory.view.AnvilView;
 
 public class PrepareAnvilListener implements Listener {
 
     @EventHandler
     public void onPrepareAnvil(PrepareAnvilEvent event) {
         ItemStack result = event.getResult();
-        AnvilInventory inventory = event.getInventory();
-        int repairCost = inventory.getRepairCost();
-        inventory.setMaximumRepairCost(Integer.MAX_VALUE);
+        AnvilView view = event.getView();
+        int repairCost = view.getRepairCost();
+        view.setMaximumRepairCost(Integer.MAX_VALUE);
         if (repairCost >= 40) {
-            inventory.setRepairCost(39);
+            view.setRepairCost(39);
         }
         if(event.getInventory().getFirstItem() != null &&
                 event.getInventory().getSecondItem() != null &&
                 event.getInventory().getFirstItem().getType() == Material.ELYTRA &&
                 event.getInventory().getSecondItem().getType() == Material.PHANTOM_MEMBRANE){
-            inventory.setRepairCost(0);
+            view.setRepairCost(0);
         }
         if (result != null) {
             if (result.getType() == Material.ELYTRA) {
@@ -51,17 +46,24 @@ public class PrepareAnvilListener implements Listener {
     public void onInventoryClick(InventoryClickEvent event) {
         if (event.getInventory().getType() == InventoryType.ANVIL) {
             AnvilInventory inventory = (AnvilInventory) event.getInventory();
-            int repairCost = inventory.getRepairCost();
-            inventory.setMaximumRepairCost(Integer.MAX_VALUE);
+            AnvilView view = (AnvilView) event.getView();
+            int repairCost = view.getRepairCost();
+            view.setMaximumRepairCost(Integer.MAX_VALUE);
             if (repairCost >= 40) {
-                inventory.setRepairCost(39);
+                view.setRepairCost(39);
             }
 
             if(inventory.getFirstItem() != null &&
                     inventory.getSecondItem() != null &&
                     inventory.getFirstItem().getType() == Material.ELYTRA &&
                     inventory.getSecondItem().getType() == Material.PHANTOM_MEMBRANE){
-                inventory.setRepairCost(0);
+                view.setRepairCost(0);
+                ItemMeta meta = inventory.getFirstItem().getItemMeta();
+                if (meta instanceof Repairable) {
+                    Repairable repairable = (Repairable) meta;
+                    repairable.setRepairCost(0);
+                    inventory.getFirstItem().setItemMeta(repairable);
+                }
             }
         }
     }
